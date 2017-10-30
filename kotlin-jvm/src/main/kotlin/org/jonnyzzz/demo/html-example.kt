32f469f17a6388eb4bcda481399ca711d9d2ac15
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-10-30 13:24:57 +0100

    html example

---
+++ kotlin-jvm/src/main/kotlin/org/jonnyzzz/demo/html-example.kt
@@ -0,0 +1,23 @@
+package org.jonnyzzz.demo
+
+import kotlinx.html.*
+import kotlinx.html.stream.createHTML
+
+val html = buildString {
+
+  createHTML().
+
+
+          html {
+            head {
+              title = "Title"
+            }
+            body {
+              h1 { +"Hello!" }
+              +"This is a most"
+              +"trivial page"
+              strong { +"ever" }
+            }
+          }
+
+}
