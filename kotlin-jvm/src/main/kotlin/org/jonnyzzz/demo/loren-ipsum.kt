a03eef232b1901e3f859d82348207cbef417fe97
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-11-08 08:39:47 +0100

    minor

--- kotlin-jvm/src/main/kotlin/org/jonnyzzz/demo/loren-ipsum.kt
+++ kotlin-jvm/src/main/kotlin/org/jonnyzzz/demo/loren-ipsum.kt
@@ -5,8 +5,11 @@ package org.jonnyzzz.demo.lorenIpsum
 
 fun main(args: Array<String>) {
   `loren ipsum` {
-    Lorem ipsum dolor sit amet `,` consectetur adipiscing elit. Ut pretium
+
+    Lorem ipsum dolor sit amet `,`
+    consectetur adipiscing elit. Ut pretium
     lorem felis `,`
+
   }
 }
 
