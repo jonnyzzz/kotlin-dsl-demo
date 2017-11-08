6fe0b8b875fb811fdd9767b9a91872fd7f4c11f5
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-11-08 17:00:58 +0100

    WIP

--- kotlin-jvm/src/main/kotlin/org/jonnyzzz/demo/main.kt
+++ kotlin-jvm/src/main/kotlin/org/jonnyzzz/demo/main.kt
@@ -4,8 +4,51 @@ package org.jonnyzzz.demo
 fun main(args: Array<String>) {
   println("Hello, Devoxx")
 
+  val client = `create the client` {
+
+    firstName = "Eugene"
+    lastName = "Petrenko"
+
+    twitter {
+      handle = "@jonnyzzz"
+    }
+
+    company {
+      name = "JetBrains"
+      city = "Munich"
+    }
+  }
+  println("Created client is: " + client.consoleString )
+
 }
 
+fun JavaClientBuilder.twitter(s: JavaTwitterBuilder.() -> Unit) {
+  twitter = JavaTwitterBuilder().apply(s).build()
+}
+
+fun JavaClientBuilder.company(s: JavaCompanyBuilder.() -> Unit) {
+  company = JavaCompanyBuilder().apply(s).build()
+}
+
+
+fun `create the client`(s: JavaClientBuilder.() -> Unit) : JavaClient {
+  val builder = JavaClientBuilder()
+
+  builder.s()
+
+  return builder.build()
+
+}
+
+fun JavaClient.toConsoleString() =
+  "${twitter.handle} ${company.name}"
+
+val JavaClient.consoleString
+   get() =  "${twitter.handle} ${company.name}"
+
+
+fun print(client: JavaClient)
+        = "${client.twitter.handle} ${client.company.name}"
 
 
 
