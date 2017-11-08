5764670af2cbde165886fe9dc0d847ff64fd463b
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-11-08 09:32:43 +0100

    wip

--- kotlin-jvm/src/main/kotlin/org/jonnyzzz/demo/main.kt
+++ kotlin-jvm/src/main/kotlin/org/jonnyzzz/demo/main.kt
@@ -3,9 +3,51 @@ package org.jonnyzzz.demo
 
 fun main(args: Array<String>) {
   println("Hello, KUG München!")
+
+
+
+  val client = buildClient {
+
+    firstName = "Eugene"
+    lastName = "Petrenko"
+
+    twitter {
+      handle = "@jonnyzzz"
+    }
+
+    company {
+      name = "JetBrains"
+      city = "Munich"
+    }
+    
+  }
+
+  println("Created client is: " + client.forConsole   )
 }
 
+fun JavaClientBuilder.twitter(setup : JavaTwitterBuilder.() -> Unit) {
+  twitter = JavaTwitterBuilder().apply(setup).build()
+}
+
+fun JavaClientBuilder.company(setup : JavaCompanyBuilder.() -> Unit) {
+  company = JavaCompanyBuilder().apply(setup).build()
+}
+
+fun buildClient(setup: JavaClientBuilder.() -> Unit): JavaClient {
+  val builder = JavaClientBuilder()
+
+  builder.setup()
+
+  return builder.build()
+}
+
+fun present(client: JavaClient)
+        = "JavaClient{${client.twitter.handle} ${client.company.name}}"
 
 
+fun JavaClient.forConsole()
+        = "JavaClient{${twitter.handle} ${company.name}}"
 
+val JavaClient.forConsole
+  get() = "JavaClient{${twitter.handle} ${company.name}}"
 
