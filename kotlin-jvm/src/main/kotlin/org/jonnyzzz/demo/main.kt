e936b3f436bfef96c431edf70e8441cd68d3a4fa
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-11-08 08:40:35 +0100

    wip

--- kotlin-jvm/src/main/kotlin/org/jonnyzzz/demo/main.kt
+++ kotlin-jvm/src/main/kotlin/org/jonnyzzz/demo/main.kt
@@ -3,8 +3,47 @@ package org.jonnyzzz.demo
 
 fun main(args: Array<String>) {
   println("Hello, KUG München!")
+
+  val client = buildClient {
+    name { "Eugene" % "Petrenko" }
+
+    twitter {
+      handle = "@jonnyzzz"
+    }
+
+    company {
+      "JetBrains" from "Munich"
+    }
+  }
+
+  println("Created client is: " + client.pres )
 }
 
+private infix fun String.from(s: String) {
+
+}
+
+fun buildClient(setup : JavaClientBuilder.() -> Unit) : JavaClient {
+  val builder = JavaClientBuilder()
+  setup(builder)
+  return builder.build()
+}
+
+fun buildTwitter(setup: JavaTwitterBuilder.() -> Unit) = JavaTwitterBuilder().apply(setup).build()
+fun buildCompany(setup: JavaCompanyBuilder.() -> Unit) = JavaCompanyBuilder().apply(setup).build()
+
+fun JavaClientBuilder.twitter(setup: JavaTwitterBuilder.() -> Unit) {
+  twitter = buildTwitter(setup)
+}
+
+fun JavaClientBuilder.company(setup: JavaCompanyBuilder.() -> Unit) {
+  company = buildCompany(setup)
+}
+
+fun JavaClient.pres() = "${this.firstName} $lastName  ${twitter.handle}"
+
+val JavaClient.pres
+  get() = pres()
 
 
 
