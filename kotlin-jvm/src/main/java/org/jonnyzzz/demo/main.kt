04efabff983d6ad46e5b979d7e5bde3ad647d65c
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-11-10 21:45:26 +0100

    step--001

--- kotlin-jvm/src/main/java/org/jonnyzzz/demo/main.kt
+++ kotlin-jvm/src/main/java/org/jonnyzzz/demo/main.kt
@@ -1,8 +1,27 @@
 package org.jonnyzzz.demo
 
+fun helloMessage(name: String) : String {
+  return "Hello, $name!"
+}
+
+fun String.hello() : String {
+  return "Hello, $this!"
+}
+
+val String.hello
+  get() : String {
+    return "Hello, $this!"
+  }
 
 fun main(args: Array<String>) {
-  println("Hello, Devoxx!")
+  println(helloMessage("Devoxx"))
+
+  for (arg in args) {
+
+    println(
+            arg.hello
+    )
+  }
 
 }
 
