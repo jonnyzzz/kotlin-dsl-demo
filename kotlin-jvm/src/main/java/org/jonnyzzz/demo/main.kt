ab448359e9bcd9fca1fb9dbdb5534afbb2d59228
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-11-11 01:27:28 +0100

    next step

--- kotlin-jvm/src/main/java/org/jonnyzzz/demo/main.kt
+++ kotlin-jvm/src/main/java/org/jonnyzzz/demo/main.kt
@@ -1,8 +1,38 @@
 @file:JvmName("MainKotlin")
+
 package org.jonnyzzz.demo
 
+
+fun searchForHello(name: String) : String? {
+  return null
+}
+
+fun guessHello(name: String): String {
+
+  val result = searchForHello(name)
+  if (result != null) {
+    return result
+  }
+
+  if (System.currentTimeMillis() > 0) return "Servus"
+
+  if (name == "jonnyzzz") return "Privet"
+  if (name == "devoxx") return "Hi"
+
+  return "Hello"
+}
+
+
+fun helloMessage(name: String,
+                 hello: String = guessHello(name)
+) = "$hello, ${name.capitalize()}!"
+
 fun main(args: Array<String>) {
-  println(helloMessage("Devoxx"))
+  println(helloMessage(
+          hello = "Privet",
+          name = "Devoxx"
+  )
+  )
 
   for (arg in args) {
 
@@ -10,5 +40,8 @@ fun main(args: Array<String>) {
             helloMessage(arg.hello)
     )
   }
+
+  val x : String? = null
+
 }
 
